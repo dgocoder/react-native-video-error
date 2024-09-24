afd2975a14d39e6ae4ff80adfe99fcbc1c8a369f
Francisco Nolla <francisco.nolla@getcenter.com>
2024-09-24 00:31:36 -0500

    video test pip

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
+import { useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Video from 'react-native-video';
 
 export default function App() {
+  const videoRef = useRef(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={videoRef}
+        source={{
+          uri: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        }}
+        style={styles.video}
+        controls
+        playInBackground
+        pictureInPicture
+        allowsExternalPlayback
+        ignoreSilentSwitch="ignore"
+        resizeMode="contain"
+        onPictureInPictureStatusChanged={(e) => console.log(e)}
+        preventsDisplaySleepDuringVideoPlayback
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  video: {
+    height: "100%",
+    width: "100%",
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
